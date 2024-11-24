1d1cd7946600a577bec01e7edaf13a90c8ddc112
venlomj <venlo.mj@hotmail.nl>
2024-11-24 21:16:34 +0100

    End of section 4

---
+++ frontend/src/features/about/AboutPage.tsx
@@ -0,0 +1,6 @@
+import { Typography } from "@mui/material";
+
+
+export default function AboutPage() {
+  return <Typography variant="h2">About page</Typography>;
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../src/app/layout/styles.css";
-import App from "./app/layout/App.tsx";
 // Material UI Fonts
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router/Routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
